cff6cce31566c285733aba2fe83a9440e3ce99ce
winEX13 <winex_13@yahoo.com>
2024-03-20 14:17:35 +0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+import asyncio
+from pyppeteer import launch
+from pyppeteer.errors import TimeoutError
 
+async def connect(userAgent: str='', proxy: str=''):
+    return await launch({
+        'executablePath': 'D:/Program Files/Google/Chrome/Application/chrome.exe',
+        'headless': True,
+        'devtools': True,
+        'autoClose': True,
+        'userDataDir': '/dev/null',
+        'args': [
+            # '--lang=en',
+            '--disable-infobars',
+            # '--start-maximized',
+            # '--no-sandbox',
+            # f'--proxy-server="https={proxy}"',
+            # f'--user-agent={userAgent}'
+        ]
+    })
+
+async def getElement(page, xpath: str, property: str):
+    try: await page.waitForXPath(xpath, timeout=3*1000)
+    except TimeoutError: return ['']
+    else: return [await (await _.getProperty(property)).jsonValue() for _ in (await page.xpath(xpath))]
+
+async def main():
+    browser = await connect()
+    page = (await browser.pages())[0]
+    
+    await page.goto('https://funpay.com/')
+    await page.waitFor(1000)
+    print(await getElement(page, "//div[@id='MPH_SCROLL_TRIGGER']", 'href'))[0]
+
+    await browser.close()
+
+if __name__ == "__main__": 
+    asyncio.run(main())
